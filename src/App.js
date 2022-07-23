10225c8d613469541afd2af50f84b6605f18da5a
Lucas Hooten <62158682+lucashooten@users.noreply.github.com>
2022-07-23 08:36:45 -0700

    Added buttons

--- src/App.js
+++ src/App.js
@@ -1,55 +1,117 @@
-import React from "react";
-import { ChakraProvider, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import React, { useCallback } from "react";
+import {
+  ChakraProvider,
+  ButtonGroup,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text
+} from "@chakra-ui/react";
 import ReactHlsPlayer from "react-hls-player";
-import bub from "./bub.png";
 
-const App = () => (
-  <ChakraProvider resetCSS>
-    <Flex
-      h="120vh"
-      w="100vw"
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="gray.800"
-      flexDirection="column"
-      overflowY="auto"
-      p={10}
-    >
+const gymLink =
+  "https://stream-ue1-bravo.dropcam.com/nexus_aac/05a6a341aba84ae8abfba1cd7c5cb036/playlist.m3u8?public=IocLXvvuyA";
+const romperLink =
+  "https://stream-uc2-alfa.dropcam.com/nexus_aac/e5b12c95e31241b481779a31c954a924/playlist.m3u8?public=qT24gocxip";
+const outsideLink =
+  "https://stream-us1-charlie.dropcam.com/nexus_aac/ccf3aa5bbe8b4489b022006a230897e3/playlist.m3u8?public=t4vRldaU0z";
+
+const App = () => {
+  const [currentCameraLink, setCurrentCameraLink] = React.useState(gymLink);
+  const setCameraToGym = () => setCurrentCameraLink(gymLink);
+  const setCameraToRomper = () => setCurrentCameraLink(romperLink);
+  const setCameraToOutside = () => setCurrentCameraLink(outsideLink);
+  const phrases = {
+    gym: "ohmydahg gym",
+    romper: "rompin",
+    outside: "hi from outside"
+  };
+  const currentPhrase = useCallback(() => {
+    if (currentCameraLink === gymLink) {
+      return phrases.gym;
+    } else if (currentCameraLink === romperLink) {
+      return phrases.romper;
+    } else if (currentCameraLink === outsideLink) {
+      return phrases.outside;
+    }
+  }, [currentCameraLink, phrases]);
+  return (
+    <ChakraProvider resetCSS>
       <Flex
-        overflow="hidden"
-        h="full"
-        maxW="600px"
-        p={10}
-        w="440px"
+        h="120vh"
+        w="100vw"
+        justifyContent="center"
+        alignItems="center"
         backgroundColor="gray.800"
         flexDirection="column"
-        pb={10}
-        mb={10}
+        overflowY="auto"
+        p={10}
       >
-        <Flex alignItems="center" justifyContent="center">
-          <Heading color="teal.500">its duh aspen cam</Heading>
-        </Flex>
-        <Flex minH="120px" justifyContent="flex-end" pos="relative">
-          <Heading
-            pos="absolute"
-            top={10}
-            right={0}
-            pr={"110px"}
-            size="sm"
-            color="whiteAlpha.500"
-          >
-            hi. am appen
-          </Heading>
-          <Image
-            top={-10}
-            right={0}
-            pos="absolute"
-            height="150px"
-            width="120px"
-            src={bub}
-          />
-        </Flex>
-        <Flex flexDirection="column" h="full">
+        <Flex
+          overflow="hidden"
+          h="full"
+          maxW="600px"
+          p={10}
+          w="440px"
+          backgroundColor="gray.800"
+          flexDirection="column"
+          pb={10}
+          mb={10}
+        >
+          <Flex alignItems="center" justifyContent="center">
+            <Heading color="teal.500">its duh aspen cam</Heading>
+          </Flex>
+          <Flex minH="120px" justifyContent="flex-end" pos="relative">
+            <Heading
+              pos="absolute"
+              top={10}
+              right={0}
+              pr={"110px"}
+              size="sm"
+              color="whiteAlpha.500"
+            >
+              hi. am appen
+            </Heading>
+            <Image
+              top={-10}
+              right={0}
+              pos="absolute"
+              height="150px"
+              width="120px"
+              src="bub.png"
+            />
+          </Flex>
+          <Flex flexDirection="column" h="full">
+            <ButtonGroup
+              size="sm"
+              isAttached
+              variant="solid"
+              mb={2}
+              colorScheme={"gray"}
+            >
+              <Button onClick={setCameraToGym}>Gym</Button>
+              <Button onClick={setCameraToRomper}>Romper</Button>
+              <Button onClick={setCameraToOutside}>Outside</Button>
+            </ButtonGroup>
+            <Text color="whiteAlpha.500" fontWeight="bold">
+              {currentPhrase()}
+            </Text>
+            <Flex
+              backgroundColor="gray.700"
+              h="full"
+              children={
+                <ReactHlsPlayer
+                  src={currentCameraLink}
+                  autoPlay={true}
+                  controls={true}
+                  width="100%"
+                  height="auto"
+                />
+              }
+            />
+          </Flex>
+          {/* <Flex flexDirection="column" h="full">
           <Text color="whiteAlpha.500" fontWeight="bold">
             hello from indoor
           </Text>
@@ -58,7 +120,7 @@ const App = () => (
             h="full"
             children={
               <ReactHlsPlayer
-                src="https://stream-uc2-alfa.dropcam.com/nexus_aac/e5b12c95e31241b481779a31c954a924/playlist.m3u8?public=qT24gocxip"
+                src=""
                 autoPlay={true}
                 controls={true}
                 width="100%"
@@ -66,7 +128,7 @@ const App = () => (
               />
             }
           />
-        </Flex>
+          </Flex> 
         <Flex flexDirection="column" h="full" mt={4}>
           <Text color="whiteAlpha.500" fontWeight="bold">
             hi from outdoor
@@ -84,10 +146,12 @@ const App = () => (
               />
             }
           />
+        </Flex> 
+        */}
         </Flex>
       </Flex>
-    </Flex>
-  </ChakraProvider>
-);
+    </ChakraProvider>
+  );
+};
 
 export default App;
